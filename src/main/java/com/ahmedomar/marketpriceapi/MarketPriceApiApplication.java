d7d83bd97b2c597fffd50351c56a2cbdb05700f7
Ahmed Omar <ahmedomar@hey.com>
2022-06-07 19:47:25 +0100

    intial commit

---
+++ src/main/java/com/ahmedomar/marketpriceapi/MarketPriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedomar.marketpriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmedomar/marketpriceapi/MarketPriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedomar.marketpriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
