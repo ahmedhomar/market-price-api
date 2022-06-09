16b68ad2f3e453061631525deb763788bc6c4c49
Ahmed Omar <ahmedomar@hey.com>
2022-06-09 10:50:53 +0100

    refactored csvhandler

--- src/main/java/com/ahmedomar/marketpriceapi/CsvFeedHandler.java
+++ src/main/java/com/ahmedomar/marketpriceapi/CsvFeedHandler.java
@@ -9,28 +9,25 @@ import java.util.List;
 public class CsvFeedHandler {
 
 
-    private CsvFeedHandler() {
-        // TODO document why this constructor is empty
-    }
-
     //    Convert a String to a single Price
-    public static Price getPriceFromCsv(String csvFile) {
-        List<Price> prices = getPricesFromCsv(csvFile);
+    public static Price getPriceFromCsv(String csvFile) throws IllegalStateException {
+        List<Price> prices = new CsvToBeanBuilder<Price>(new StringReader(csvFile))
+                .withType(Price.class) // <-- this is the important line
+                .withIgnoreLeadingWhiteSpace(true) //ignore leading whitespace
+                .build() //build the CSVReader
+                .parse(); //parse the CSV file
         return prices.get(0); //return the first price in the list
     }
 
-// Convert CSV to List of prices
-    public static List<Price> getPricesFromCsv(String csvFile) {
-        CSVReader reader = new CSVReader(new StringReader(csvFile));
-        return new CsvToBeanBuilder<Price>(reader)
-                .withType(Price.class)
-                .withIgnoreLeadingWhiteSpace(true)
-                .build()
+    // Convert CSV to List of prices
+    public static List<Price> getPricesFromCsv(String csvFile) throws IllegalStateException {
+        CSVReader reader = new CSVReader(new StringReader(csvFile));  //create a CSVReader
+        return new CsvToBeanBuilder<Price>(reader) //create a CsvToBeanBuilder
+                .withType(Price.class) // <-- this is the important line
+                .withIgnoreLeadingWhiteSpace(true) //ignore leading whitespace
+                .build() //build the CSVReader
                 .parse(); //returns a list of prices
     }
-
-
-
 
 
 }
